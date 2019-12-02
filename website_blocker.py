3d6002f8e26ebc9f7de45f9400392cb2bfefbd6f
brucelostis <brucelostis@gmail.com>
2019-12-02 23:38:03 +0100

    Initial commit

---
+++ website_blocker.py
@@ -0,0 +1,23 @@
+import time
+from datetime import datetime as dt
+
+host_path = "hosts"
+redirect = "127.0.0.1"
+
+website_list = ["www.facebook.com", "facebook.com", "yahoo.com", "www.bing.com"]
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+        print("Working Hours..")
+        with open(host_path, '+r') as file:
+            content = file.read()
+            print(content)
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+ " "+website+"\n")
+
+    else:
+        print("Fun Hours..")
+    time.sleep(5)
